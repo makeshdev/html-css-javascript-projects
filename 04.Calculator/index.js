0e8a997d1692989ec281e2bd738017f243ec66a6
Makesh Dev <makeshdev1995@gmail.com>
2024-04-07 09:43:33 +0530

    calculator

--- 04.Calculator/index.js
+++ 04.Calculator/index.js
@@ -1,3 +1,20 @@
-let equal_press = 0;
+let button_input = document.getElementById("input");
 
-//Refer all buttons excluding AC and DEL
+const display = (num) => {
+  button_input.value += num;
+};
+
+const calculate = () => {
+  try {
+    button_input.value = eval(button_input.value);
+  } catch (error) {
+    alert("Invalid");
+  }
+};
+
+function clears() {
+  button_input.value = " ";
+}
+function del() {
+  button_input.value = button_input.value.slice(0, -1);
+}
